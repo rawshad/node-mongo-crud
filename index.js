7b719a1db1a6de1cea16d38f63b9a9c6d4d2501d
rawshad <rawshad@outlook.com>
2022-03-23 22:48:35 +0600

    commited

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const password = "yjgTvAmGCoqqqErX";
+const uri = "mongodb+srv://mycrudapplication:yjgTvAmGCoqqqErX@cluster0.6vjc7.mongodb.net/crudAppDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+
+client.connect(err => {
+  const collection = client.db("crudAppDatabase").collection("appData");
+  app.get("/products", (req, res) => {
+    collection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    collection.insertOne(product)
+    .then(result=>{
+        console.log("data added successfully.");
+        res.send("successfull");
+      })
+  })
+});
+
+
+app.listen(5000);
